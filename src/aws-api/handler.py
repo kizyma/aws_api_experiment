4546a354e563e1854ed4195b5436865e0ff8538b
Yaroslav Kizyma <yaroslav@skylum.com>
2021-12-06 17:54:35 +0200

    - try & create first event create handler function - transform utils into a python package

--- src/aws-api/handler.py
+++ src/aws-api/handler.py
@@ -1,24 +1,42 @@
 import json
+import logging
+import boto3
+import datetime
+from src.utils import dynamo_db_serializer
+from src.utils.pydantic_datamodel import GenericEvent
+from pydantic import ValidationError
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+dynamodb = boto3.client('dynamodb')
+table_name = 'events'
 
 
-def hello(event, context):
-    body = {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "input": event
-    }
+def create(event, context):
+    logger.info(f'Incoming request is: {event}')
 
+    # Set the default error response
     response = {
-        "statusCode": 200,
-        "body": json.dumps(body)
+        "statusCode": 500,
+        "body": "An error occurred while creating post."
     }
+    post_str = event['body']
+    try:
+        event = GenericEvent.parse_raw(post_str)
+    except ValidationError:
+        response = {
+            "statusCode": 400,
+            "body": "Bad Request. Data validation has been failed."
+                    "Please check input data types & make sure they correspond to the requirements."
+        }
+
+    res = dynamodb.put_item(
+        TableName=table_name, Item=dynamo_db_serializer.to_item(event))
+
+    # If creation is successful
+    if res['ResponseMetadata']['HTTPStatusCode'] == 200:
+        response = {
+            "statusCode": 201,
+        }
 
     return response
-
-    # Use this code if you don't use the http event with the LAMBDA-PROXY
-    # integration
-    """
-    return {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "event": event
-    }
-    """
